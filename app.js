15773ae8fad5843388cfd1a9235bfe0569bc2a94
Alessandro Franceschetti <alessandro.franceschetti@csi.it>
2017-01-30 10:01:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -51,17 +51,12 @@ var pantoneSpring2016 = {name:'Pantone Spring 2016', desc: ' <a href="http://www
 				};				
 
 
-var nimberPalette = {name: 'Nimber Palette', desc: 'Palette used by <a href="https://www.nimber.com/">Nimber</a>', palette: 
+	var nimberPalette = {name: 'Nimber Palette', desc: 'Palette used by <a href="https://www.nimber.com/">Nimber</a>', palette: 
 			 [{'name':'Accent Red','colors':['c83321','a41807']},
 			  {'name':'White','colors':['ffffff','f2f2f2']},
-			  {'name':'Accent Red','colors':['363C43','21262d']},
-			  {'name':'Primary Gray','colors':['808A9d','6d788c']},
-			  {'name':'Secondary Gray','colors':['b8c0ce','95a0b4']},
-			  {'name':'Tertiary Gray','colors':['fec411','e5a904']},
-			  {'name':'Yellow','colors':['ffb900','e5a904']},
-			  {'name':'Rating Yellow','colors':['72c700','5a9d00']},
-			  {'name':'Error Red','colors':['f4511e','c73608']},
-			  {'name':'Info Blue','colors':['46a6eb','1979be']}]};	
+			  {'name':'Gray','colors':['363C43','21262d','808A9d','6d788c''b8c0ce','95a0b4']},
+			  {'name':'Yellow','colors':['fec411','e5a904','ffb900','e5a904']},
+			  {'name':'Info colors','colors':['72c700','5a9d00','f4511e','c73608','46a6eb','1979be']}]};	
 				
 
 var palettes =[tangoPalette, alePalette, metroPalette,nimberPalette];
