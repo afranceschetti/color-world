768136d575a64191ab06294fd7bb0821ac4a7dc5
Alessandro Franceschetti <alessandro.franceschetti@csi.it>
2017-01-30 10:14:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ var metroPalette = 	{name:'Windows Phone', desc: 'Palette metro style used in wi
 						{'name':'Sienna','colors':['647687','76608a']}	
 					]
 				};
-				
+
+
 var pantoneSpring2016 = {name:'Pantone Spring 2016', desc: ' <a href="http://www.pantone.com/pages/fcr/?season=spring&year=2016&pid=11" target="_blank" >Spring 2016</a>: A Transporting and Transformative Canvas', palette: 
 					[{'name':'Rose Quartz','colors':['F7CAC9']},
 						{'name':'Peach Echo','colors':['F7786B']},
@@ -48,7 +49,10 @@ var pantoneSpring2016 = {name:'Pantone Spring 2016', desc: ' <a href="http://www
 						{'name':'Green Flash','colors':['79C753']},
 					]
 				};				
-				
+
+
+
+
 var palettes =[tangoPalette, alePalette, metroPalette];
 
 
@@ -58,10 +62,18 @@ var jackProductionPalette = { name: 'Jack Production', desc: 'Mini palette found
 var ios7Palette = { name: 'iOs 7 Palette', desc: 'Palette used on iPhone in iOs7', 
 						palette: [{colors:['8e8e93','ff2252','ff342f','ff9422','ffcc34']},
 						{colors:['2ada70','48c9f8','10abda','007afa','5855d1']}]};
+
+
+var nimberPalette = {name: 'Nimber Palette', desc: 'Palette used by <a href="https://www.nimber.com/" target="_blank">Nimber</a>', palette: 
+		 [{'colors':['c83321','ffffff','363C43','808A9d','b8c0ce']},
+		 {'colors':['a41807','f2f2f2','21262d','6d788c','95a0b4']},
+		 {'colors':['fec411','ffb900','72c700','f4511e','46a6eb']},
+		 {'colors':['e5a904','e5a904','5a9d00','c73608','1979be']}]};	
+			  
 var pantoneSpring2016Small = {name:'Pantone Spring 2016', desc: ' <a href="http://www.pantone.com/pages/fcr/?season=spring&year=2016&pid=11" target="_blank" >Spring 2016</a>: A Transporting and Transformative Canvas', 
 palette: [{'colors':['F7CAC9','F7786B','91A8D0','034F84','FAE03C']},{'colors':['98DDDE','9896A4','DD4132','B18F6A','79C753']}]};					
 
-var smallPalettes =[ios7Palette, jackProductionPalette, pantoneSpring2016Small];					
+var smallPalettes =[ios7Palette, jackProductionPalette, pantoneSpring2016Small,nimberPalette];					
 
 $(document).ready(function() {
 	init();
@@ -106,7 +118,7 @@ function init(){
 	for (i = 0; i < smallPalettes.length; i++) {
 		var paletteId = "_s" + i;
 		smallPaletteHtml += createPaletteHtml(smallPalettes[i],paletteId);
-		navPaletteMenuItems += "<li><a href='#" + paletteId + "'>" + palettes[i].name + "</a></li>";
+		navPaletteMenuItems += "<li><a href='#" + paletteId + "'>" + smallPalettes[i].name + "</a></li>";
 	}
 	$("#small-palettes").html(smallPaletteHtml);
 	$("#nav-palette-dropdown").html(navPaletteMenuItems);
